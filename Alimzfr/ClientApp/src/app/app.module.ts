82e3732e3edca561697b9f753635524117c92abf
Ali Mozaffari <alimozaffari13701370@gmail.com>
2020-03-21 13:34:35 +0430

    test commit from webstorm

--- Alimzfr/ClientApp/src/app/app.module.ts
+++ Alimzfr/ClientApp/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountComponent } from './authentication/account/account.component';
-import { RegisterComponent } from './authentication/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
