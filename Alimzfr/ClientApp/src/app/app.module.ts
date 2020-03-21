157974c8a5b26e2c3e96b2d6eb10855479348399
Ali Mozaffari <alimozaffari13701370@gmail.com>
2020-03-21 12:51:56 +0430

    First commit

---
+++ Alimzfr/ClientApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccountComponent } from './authentication/account/account.component';
+import { RegisterComponent } from './authentication/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
