e8a23d47993ae44c44ff45ed1b079e03dbd14e3b
Ali Mozaffari <alimozaffari13701370@gmail.com>
2020-03-23 12:18:17 +0430

    create routing and add bootstrap in project

--- Alimzfr/ClientApp/src/app/app-routing.module.ts
+++ Alimzfr/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './features/home/home.component';
+import {AboutComponent} from './features/about/about.component';
+import {BlogComponent} from './features/blog/blog.component';
+import {ContactComponent} from './features/contact/contact.component';
+import {EducationComponent} from './features/education/education.component';
+import {ExperienceComponent} from './features/experience/experience.component';
+import {SkillsComponent} from './features/skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: HomeComponent, path: ':lang?'},
+  {component: AboutComponent, path: ':lang?/About'},
+  {component: BlogComponent, path: ':lang?/Blog'},
+  {component: ContactComponent, path: ':lang?/Contact'},
+  {component: EducationComponent, path: ':lang?/Education'},
+  {component: ExperienceComponent, path: ':lang?/Experience'},
+  {component: SkillsComponent, path: ':lang?/Skills'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Alimzfr/ClientApp/src/app/app.module.ts
+++ Alimzfr/ClientApp/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './shareComponents/template/template.component';
+import { SidebarComponent } from './shareComponents/template/sidebar/sidebar.component';
+import { ContentComponent } from './shareComponents/template/content/content.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { EducationComponent } from './features/education/education.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TemplateComponent,
+    SidebarComponent,
+    ContentComponent,
+    SkillsComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
